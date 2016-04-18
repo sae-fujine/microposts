8d953129941967e72bf95a35ffdca3daadc1920b
sae fujine <sae-fujine@united.jp>
2016-04-18 08:08:31 +0000

    0418-8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
+<<<<<<< HEAD
   has_many :microposts
 end
+=======
+  before_save { self.email = self.email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+end
+>>>>>>> user-profile
