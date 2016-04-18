2e00093f937c10afc406efacf2c96f409e8e228d
sae fujine <sae-fujine@united.jp>
2016-04-18 09:22:50 +0000

    add posting microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :microposts
-end
-=======
   before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,5 +6,5 @@ end
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  has_many :microposts
 end
->>>>>>> user-profile
