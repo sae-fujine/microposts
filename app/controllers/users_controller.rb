7d2c166cdeab3bbe5ef06b709868fa92b909d051
sae fujine <sae-fujine@united.jp>
2016-04-19 01:54:53 +0000

    add followings/followers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,28 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:edit, :update]
-before_action :correct_user,   only: [:edit, :update]#これ追加！
-
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :followings, :followers]
+    before_action :correct_user,   only: [:edit, :update]
+   
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc)
+  #  @microposts = @user.microposts.order(created_at: :desc).page(params[:page]).per(10)
+  end
+  
+  def followings
+    @title = "Following"
+    @user = User.find(params[:id])
+    @following_users = @user.following_users.page(params[:page]).per(10)
   end
 
+  def followers
+    @title = "Follower"
+    @user = User.find(params[:id])
+    @follower_users = @user.follower_users.page(params[:page]).per(10)
+  end
+  
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -24,7 +36,7 @@ before_action :correct_user,   only: [:edit, :update]#これ追加！
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -34,14 +46,15 @@ before_action :correct_user,   only: [:edit, :update]#これ追加！
     end
   end
 
+  
+  
   private
 
   def user_params
-    params.require(:user).permit(:name, :area, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :area, :email, :password, :password_confirmation)
   end
   
-    # beforeフィルター
+  # beforeフィルター
 
     # ログイン済みユーザーかどうか確認
     def logged_in_user
@@ -56,4 +69,8 @@ before_action :correct_user,   only: [:edit, :update]#これ追加！
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+    
+  
+
+  
 end
